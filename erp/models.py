ffc1b866a72916ecd4dc65263ebd9e73c3a173d0
NACHO <ignacio.torres.g@mail.pucv.cl>
2023-11-13 16:17:01 -0300

    Commit Error1

--- erp/models.py
+++ erp/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, nombre=None, **extra_fields):
+    def create_user(self, email, password=None, nombre='Nombre Predeterminado', **extra_fields):
         if not email:
             raise ValueError('El campo de correo electrónico es obligatorio')
         email = self.normalize_email(email)
-        user = self.model(email=email, nombre=nombre, **extra_fields)  # Agrega el campo "nombre"
+        user = self.model(email=email, nombre=nombre, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, nombre='Administrador', **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -20,7 +20,7 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Los superusuarios deben tener is_superuser=True.')
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email, password, nombre=nombre, **extra_fields)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
--- servicesERP/urls.py
+++ servicesERP/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path, include
 from django.contrib import admin
-from erp.views import home, login_view, menu_view, procesar_formulario
+from erp.views import home, login_view, menu_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('erp/', include('erp.urls')),  # Elimina 'namespace='erp''
     path('erp/menu/', menu_view, name='menu'),
-    path('erp/procesar-formulario/', procesar_formulario, name='procesar-formulario'),
     path('login/', login_view, name='login'),
     # Otras rutas aquí...
 ]
